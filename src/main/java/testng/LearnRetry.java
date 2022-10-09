a0216913bbaaf031d1c1fb0b16ec131c696886ca
hp <hp@DESKTOP-MA100A2>
2022-10-09 17:53:12 +0530

    Listner

---
+++ src/main/java/testng/LearnRetry.java
@@ -0,0 +1,13 @@
+package testng;
+import java.util.NoSuchElementException;
+
+import org.testng.annotations.Test;
+
+public class LearnRetry {
+	//@Test(retryAnalyzer=RetryFailedTestCases.class)
+	//@Test(retryAnalyzer=RetryFailedTestCases.class)
+		//public void CreateOpportunity() {
+		//	System.out.println("create opportuniyu");
+		//	throw new NoSuchElementException();
+		}
+
---
+++ src/main/java/testng/RetryFailedTestCases.java
@@ -0,0 +1,16 @@
+package testng;
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+public class RetryFailedTestCases implements IRetryAnalyzer{
+	
+	int retryCount=0;
+	public boolean retry(ITestResult result) {
+		if(retryCount<3) {
+			retryCount++;
+			return true;
+			
+		}
+		return false;
+	}
+} 
+
